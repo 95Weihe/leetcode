a499df7349381d0aee4ff14246393ceae90ab06d
95Weihe <121376706+95Weihe@users.noreply.github.com>
2023-01-01 21:15:11 +0800

    Create 003.java

---
+++ java/003.java
@@ -0,0 +1,25 @@
+class Solution {
+/*
+  给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+*/
+
+    public int lengthOfLongestSubstring(String s) {
+        // 记录字符上一次出现的位置
+        int[] last = new int[128];
+        for(int i = 0; i < 128; i++) {
+            last[i] = -1;
+        }
+        int n = s.length();
+
+        int res = 0;
+        int start = 0; // 窗口开始位置
+        for(int i = 0; i < n; i++) {
+            int index = s.charAt(i);
+            start = Math.max(start, last[index] + 1);
+            res   = Math.max(res, i - start + 1);
+            last[index] = i;
+        }
+
+        return res;
+    }
+}
