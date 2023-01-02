87a998ab95d2a0f41c9f98a1655e12127416cecc
95Weihe <121376706+95Weihe@users.noreply.github.com>
2023-01-02 13:32:16 +0800

    Add files via upload

--- java/003.java
+++ java/003.java
@@ -6,20 +6,15 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         // 记录字符上一次出现的位置
         int[] last = new int[128];
-        for(int i = 0; i < 128; i++) {
-            last[i] = -1;
-        }
-        int n = s.length();
-
+        int len = s.length();
+        int start = 0;
         int res = 0;
-        int start = 0; // 窗口开始位置
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < len; i++){
             int index = s.charAt(i);
-            start = Math.max(start, last[index] + 1);
-            res   = Math.max(res, i - start + 1);
-            last[index] = i;
+            start = Math.max(start, last[index]);
+            res   = Math.max(res, i - last[index] + 1);
+            last[index] = i + 1;
         }
-
         return res;
     }
 }
