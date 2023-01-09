76e18965a85c4a73eeb2c82e3100d1feb04ce809
root <1349766877@qq.com>
2023-01-09 21:36:50 +0800

    update

--- go/001.go
+++ go/001.go
@@ -1,10 +1,8 @@
 package main
-
 /*
 	给定一个整数数组 nums 和一个整数目标值 target，
 	请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
 	2 7 11 5      9
-
 */
 func twoSum(nums []int, target int) []int {
 	table := make(map[int]int)
