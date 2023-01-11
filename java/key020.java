d24838515bbf81f30e2eb969bfa4390f24026932
root <1349766877@qq.com>
2023-01-11 21:42:08 +0800

    update

--- java/key020.java
+++ java/key020.java
@@ -1,26 +1,31 @@
-public boolean isValid(String s) {
-    /*
-给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+class Solution {
+    /**
+     给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
 
-有效字符串需满足：
-左括号必须用相同类型的右括号闭合。
-左括号必须以正确的顺序闭合。
-每个右括号都有一个对应的相同类型的左括号。
-*/
-    if(s.isEmpty())
-        return true;
-    Stack<Character> stack=new Stack<Character>();
-    for(char c:s.toCharArray()){
-        if(c=='(')
-            stack.push(')');
-        else if(c=='{')
-            stack.push('}');
-        else if(c=='[')
-            stack.push(']');
-        else if(stack.empty()||c!=stack.pop())
-            return false;
+     有效字符串需满足：
+
+     左括号必须用相同类型的右括号闭合。
+     左括号必须以正确的顺序闭合。
+     每个右括号都有一个对应的相同类型的左括号
+
+     */
+    public boolean isValid(String s) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char str = s.charAt(i);
+            if (str == '(' || str == '[' || str == '{') {
+                deque.push(str);
+            } else {
+                if(deque.size() == 0) return false;
+                if (str == ')' && deque.pop() != '(') {
+                    return false;
+                } else if (str == ']' && deque.pop() != '[') {
+                    return false;
+                } else if (str == '}' && deque.pop() != '{') {
+                    return false;
+                }
+            }
         }
-    if(stack.empty())
-        return true;
-    return false;
+        return deque.size() == 0;
+    }
 }
