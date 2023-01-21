0a613f431f22ab2d512f21f27d8a2b85ecd4eab5
root <1349766877@qq.com>
2023-01-21 21:04:15 +0800

    update

---
+++ java/key070.java
@@ -0,0 +1,14 @@
+class Solution {
+    /**假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+     每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？ */
+    public int climbStairs(int n) {
+        int p = 0, q = 0, r = 1;
+        for (int i = 1; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+    }
+}
