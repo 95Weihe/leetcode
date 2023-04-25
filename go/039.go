9f8f7b65bca30d55231c97b93bd6a4e6c78bfb2e
longrl <291008298@qq.com>
2023-04-25 21:52:00 +0800

    add: 039 golang code

--- go/039.go
+++ go/039.go
@@ -3,19 +3,34 @@ package main
 import "sort"
 
 /**
-组合总和
+组合总和  回溯 + 剪枝
 */
 func combinationSum(candidates []int, target int) [][]int {
 	if len(candidates) == 0 {
 		return [][]int{}
 	}
-	c, res := []int{}, [][]int{}
-	findcombinationSum(candidates, target, 0, c, &res)
+	var c []int
+	var res [][]int
 	sort.Ints(candidates)
-
+	dfsSum(candidates, target, 0, c, &res)
 	return res
 }
 
-func findcombinationSum(nums []int, target, index int, c []int, res *[][]int) {
-	
+func dfsSum(nums []int, target, index int, c []int, res *[][]int) {
+	if target <= 0 {
+		if target == 0 {
+			b := make([]int, len(c))
+			copy(b, c)
+			*res = append(*res, b)
+		}
+		return
+	}
+	for i := index; i < len(nums); i++ {
+		if target-nums[i] < 0 {
+			break
+		}
+		c = append(c, nums[i])
+		dfsSum(nums, target-nums[i], i, c, res)
+		c = c[:len(c)-1]
+	}
 }
